6775d550ab751f73eb56ccfe9fb937b72196e1a1
Leonardo Vivanco <solyle@gmail.com>
2026-04-16 16:27:21 -0400

    CIRD actualizado para ingresar a SSH desde una IP en especifica
    Aumento de seguridad al insertar una IP en especifica

--- ec2.tf
+++ ec2.tf
@@ -13,7 +13,7 @@ resource "aws_security_group" "ssh_access" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Permitir desde cualquier dirección IPv4
+    cidr_blocks = ["152.230.70.146/32"] # Permitir desde cualquier dirección IPv4
   }
 
   egress {
