298e11b4898a2729b58a5a4ef9a45bf02e65566c
Leonardo Vivanco <solyle@gmail.com>
2026-04-16 15:47:49 -0400

    Update VPC CIDR block from 10.0.0.0/16 to 10.1.0.0/16
    se cambia el CIDR debido a que la evaluación pide explícitamente la "10.1.0.0/16"

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 # Crear la VPC
 resource "aws_vpc" "mi_vpc" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = "10.1.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
